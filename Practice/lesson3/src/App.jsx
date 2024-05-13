1fa1d2082dca431ae9ffe8b6058aee043691f0a9
khotranpc <khotranpc603@gmail.com>
2024-05-13 21:15:45 +0700

    init

--- Practice/lesson3/src/App.jsx
+++ Practice/lesson3/src/App.jsx
@@ -3,15 +3,41 @@ import png001 from './assets/001.png';
 import png002 from './assets/002.png';
 import png006 from './assets/006-Gmax.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // 5p viết 4 state tương ứng cho 4 component card
+  const [bulbasaur, setBulbasaur] = useState({
+    name: 'Bulbasaur',
+    image: png001,
+    pokemonClass: 'Grass',
+    age: 10
+  });
+  const [ivysaur, setIvysaur] = useState({
+    name: 'ivysaur',
+    image: png002,
+    pokemonClass: 'Grass',
+    age: 12
+  });
+  const [venusaur, setVenusaur] = useState({
+    name: 'venusaur',
+    image: png002,
+    pokemonClass: 'Grass',
+    age: 57
+  });
+  const [charmander, setCharmander] = useState({
+    name: 'charmander',
+    image: png006,
+    pokemonClass: 'Fire',
+    age: 30
+  });
 
   return (
     <div className='listPokemonCard'>
-      <PokemonCard name="Bulbasaur" image={png001} pokemonClass="Grass" age={10} />
-      <PokemonCard name="Ivysaur" image={png002} pokemonClass="Grass" age={12} />
-      <PokemonCard name="Venusaur" image={png002} pokemonClass="Grass" age={57} />
-      <PokemonCard name="Charmander" image={png006} pokemonClass="Fire" age={30} />
+      <PokemonCard name={bulbasaur.name} image={bulbasaur.image} pokemonClass={bulbasaur.pokemonClass} age={bulbasaur.age} />
+      <PokemonCard name={ivysaur.name} image={ivysaur.image} pokemonClass={ivysaur.pokemonClass} age={ivysaur.age} />
+      <PokemonCard name={venusaur.name} image={venusaur.image} pokemonClass={venusaur.pokemonClass} age={venusaur.age} />
+      <PokemonCard name={charmander.name} image={charmander.image} pokemonClass={charmander.pokemonClass} age={charmander.age} />
     </div>
   )
 }
